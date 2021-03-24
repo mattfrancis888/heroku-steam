eb77ceadfd836bca35e954940d383c53b31df735
Matthew Francis <mattfrancis888@gmail.com>
2021-03-23 22:10:56 -0400

    Added build folder with react spring animation for featured carousel.

--- backend-build/databasePool.js
+++ backend-build/databasePool.js
@@ -10,16 +10,16 @@ console.log("USER", process.env.user);
 console.log("database", process.env.database);
 console.log("password", process.env.password);
 var pool = new pg_1.Pool({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: 5432,
-    // user: process.env.elephantSQLUser,
-    // host: process.env.elephantSQLServer,
-    // database: process.env.elephantSQLDatabase,
-    // password: process.env.elephantSQLPassword,
+    // user: process.env.user,
+    // host: process.env.host,
+    // database: process.env.database,
+    // password: process.env.password,
     // port: 5432,
+    user: process.env.elephantSQLUser,
+    host: process.env.elephantSQLServer,
+    database: process.env.elephantSQLDatabase,
+    password: process.env.elephantSQLPassword,
+    port: 5432,
 });
 pool.on("error", function (err) {
     // tslint:disable-next-line no-console
--- databasePool.ts
+++ databasePool.ts
@@ -9,17 +9,17 @@ console.log("USER", process.env.user);
 console.log("database", process.env.database);
 console.log("password", process.env.password);
 const pool = new Pool({
-    user: process.env.user,
-    host: process.env.host,
-    database: process.env.database,
-    password: process.env.password,
-    port: 5432,
-
-    // user: process.env.elephantSQLUser,
-    // host: process.env.elephantSQLServer,
-    // database: process.env.elephantSQLDatabase,
-    // password: process.env.elephantSQLPassword,
+    // user: process.env.user,
+    // host: process.env.host,
+    // database: process.env.database,
+    // password: process.env.password,
     // port: 5432,
+
+    user: process.env.elephantSQLUser,
+    host: process.env.elephantSQLServer,
+    database: process.env.elephantSQLDatabase,
+    password: process.env.elephantSQLPassword,
+    port: 5432,
 });
 pool.on("error", (err) => {
     // tslint:disable-next-line no-console
